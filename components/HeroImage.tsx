26f421cf79ce0c7873b9c7094a553c95092048a7
Eric Nguyen <15enguyen4@gmail.com>
2023-07-31 13:40:04 -0700

    testing images for parallax effect

--- components/HeroImage.tsx
+++ components/HeroImage.tsx
@@ -3,22 +3,29 @@ import ufo from '@/public/ufo-spaceship.jpeg'
 import space from '@/public/space-palm.jpg'
 import landing from '@/public/landing.jpg'
 import Image from 'next/image'
+import rocketShip from '@/public/rocket-ship-cropped.png'
 
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 function HeroImage() {
-
-  const ref = useRef();
-
+  const alignCenter = { display: 'flex', alignItems: 'center' }
   return (
-    <div>
-      <Parallax pages={4} ref={ref}>
-        <ParallaxLayer
+     <div
+      style={{
+        backgroundImage: `url('/space.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        height: '100vh', // Set the height of the parallax container
+      }}
+    >
+      <Parallax pages={2} >
+        {/* <ParallaxLayer
           offset={0}
-          speed={1.5}
+          speed={0.5}
           factor={2}
           style={{
-            backgroundImage: `url('/space-palm.jpg')`,
+            backgroundImage: `url('/space.jpg')`,
             backgroundSize: 'cover',
           }} 
         />
@@ -28,32 +35,32 @@ function HeroImage() {
           speed={1}
           factor={4}
           style={{
-            backgroundImage: `url('/landing.jpg')`,
+            backgroundImage: `url('/space-2.jpg')`,
             backgroundSize: 'cover',
           }}
-        />
+        /> */}
 
-        {/* <ParallaxLayer
-          sticky={{ start: 0.9, end: 2.5 }}
-          style={{ textAlign: 'center' }}
+        <ParallaxLayer
+          sticky={{ start: 1, end: 3 }}
+          style={{  ...alignCenter, justifyContent: 'flex-start'}}
         >
-        </ParallaxLayer> */}
+          <h2 className='ml-15 text-white'>Take a trip to outer space</h2>
+        </ParallaxLayer>
         
         <ParallaxLayer
-          offset={0.2}
-          speed={0.05}
-          onClick={() => ref.current.scrollTo(3)}
+          offset={1.5}
+          speed={1.5}
+          style={{ ...alignCenter, justifyContent: 'flex-end' }}
         >
-          <h2 className='flex justify-center text-white'>Take a trip to outer space</h2>
+          <Image className='mr-15' src={rocketShip} alt='rocket-ship'  width={300} height={200}/>
         </ParallaxLayer>
 
-        <ParallaxLayer
+        {/* <ParallaxLayer
           offset={3}
           speed={2}
-          onClick={() => ref.current.scrollTo(0)}
         >
           <h2 className='flex justify-center text-white'>Crash land on the moon!</h2>
-        </ParallaxLayer>
+        </ParallaxLayer> */}
       </Parallax>
     </div>
   )
